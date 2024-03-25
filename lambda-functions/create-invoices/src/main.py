d99132ca11b182db1cfe626e8a5100a20ebf5a1a
thethiago27 <thethiago27@gmail.com>
2024-03-25 14:12:54 -0400

    feat: start project

---
+++ lambda-functions/create-invoices/src/main.py
@@ -0,0 +1,58 @@
+import os
+from datetime import datetime, timedelta
+import boto3
+import requests
+
+
+def lambda_handler(event, context):
+    amount = event.get('amount')
+    qr_code_id, qr_code_payload = create_pix_code(amount)
+    save_invoice(amount, qr_code_id, qr_code_payload, event.get('user_id'))
+
+    return {
+        'qr_code_id': qr_code_id,
+        'qr_code_payload': qr_code_payload
+    }
+
+
+def save_invoice(amount, qr_code_id, qr_code_payload, user_id):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('invoices')
+
+    item = {
+        'user_id': user_id,
+        'amount': amount,
+        'qr_code_id': qr_code_id,
+        'qr_code_payload': qr_code_payload,
+        'status': 'pending'
+    }
+
+    table.put_item(Item=item)
+
+
+def create_pix_code(amount):
+    url = "https://api.asaas.com/v3/pix/qrCodes/static"
+    headers = {
+        "Content-Type": "application/json",
+        "access_token": os.getenv('asaas_token')
+    }
+
+    expiration_date = datetime.now() + timedelta(minutes=10)
+    expiration_date_str = expiration_date.strftime("%Y-%m-%d %H:%M:%S")
+
+    data = {
+        "addressKey": os.getenv('pix_key'),
+        "description": "Recarga de saldo",
+        "value": amount,
+        "format": "PAYLOAD",
+        "expirationDate": expiration_date_str
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    response_json = response.json()
+
+    qr_code_id = response_json.get('id')
+    qr_code_payload = response_json.get('payload')
+
+    return qr_code_id, qr_code_payload
---
+++ lambda-functions/get-user-balance/src/main.py
@@ -0,0 +1,12 @@
+import boto3
+
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('user_balance')
+    response = table.get_item(Key={'user_id': event['user_id']})
+    item = response.get('Item')
+    if item:
+        return {'balance': item.get('balance')}
+    else:
+        return None
