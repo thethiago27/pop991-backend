95d16ce099c01c8998ef991bd1d86d7a57b49dde
thethiago27 <thethiago27@gmail.com>
2024-03-26 18:19:59 -0400

    feat: add authorizer

--- lambda_functions/authorizer/src/main.py
+++ lambda_functions/authorizer/src/main.py
@@ -14,19 +14,16 @@ def lambda_handler(event, context):
         if not token:
             return response
 
-        if token == 'Bearer token':
-            response = {
-                "isAuthorized": True,
-                "context": {
-                    "stringKey": "value",
-                    "numberKey": 1,
-                    "booleanKey": True,
-                    "arrayKey": ["user_id", "1234"],
-                    "mapKey": {"user_id": "1234"}
-                }
+        response = {
+            "isAuthorized": True,
+            "context": {
+                "stringKey": "value",
+                "numberKey": 1,
+                "booleanKey": True,
+                "arrayKey": ["user_id", "1234"],
+                "mapKey": {"user_id": "1234"}
             }
-        else:
-            return response
+        }
     except BaseException:
         print("No token provided")
         return response
