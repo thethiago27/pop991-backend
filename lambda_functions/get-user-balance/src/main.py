abb7ee4701e7092f016d9d2ce9598adb4a502cc4
thethiago27 <thethiago27@gmail.com>
2024-03-27 14:06:58 -0400

    feat: add authorizer

--- lambda_functions/get-user-balance/src/main.py
+++ lambda_functions/get-user-balance/src/main.py
@@ -3,8 +3,9 @@ import boto3
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
+    user_id = context['authorizer']['user_id']
     table = dynamodb.Table('balance')
-    response = table.get_item(Key={'user_id': context['user_id']})
+    response = table.get_item(Key={'user_id': user_id})
     item = response.get('Item')
     if item:
         return {'balance': item.get('balance')}
