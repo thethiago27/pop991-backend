a76754e768404f7ad40aa0071aa1e94403d8566c
thethiago27 <thethiago27@gmail.com>
2024-04-01 19:57:42 -0400

    feat: create new functions

--- lambda_functions/get-user-balance/src/main.py
+++ lambda_functions/get-user-balance/src/main.py
@@ -14,9 +14,22 @@ def lambda_handler(event, context):
     response = table.get_item(Key={'user_id': principal_id})
     item = response.get('Item')
     if item:
-        return {'balance': item.get('balance')}
+        return {
+            'body': json.dumps({
+                'balance': item.get('balance'),
+            }),
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': "true",
+            }
+        }
     else:
         return {
             'statusCode': 404,
-            'body': json.dumps({'message': 'User not found', event: event})
+            'body': json.dumps({'message': 'User not found', event: event}),
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': "true",
+            }
         }
