175257ee599a093cd5ef67660cd593b67c321ddb
thethiago27 <thethiago27@gmail.com>
2024-04-15 13:28:37 -0400

    feat: create new functions

--- lambda_functions/create-invoices/src/main.py
+++ lambda_functions/create-invoices/src/main.py
@@ -47,7 +47,7 @@ def notify_transaction(amount, user_id):
                 'StringValue': user_id
             },
             'type': {
-                'DataType': 'String',
+                'DataType': 'Number',
                 'StringValue': 'deposit'
             }
         },
